1c58f4bee6a0212e1dde31ac053570496174ed29
JJ <jj.stankowicz@gmail.com>
2024-08-06 23:32:50 -0400

    Create create_tests.py.

---
+++ dl2np/iac/create_tests.py
@@ -0,0 +1,18 @@
+from dl2np.utils import get_config
+from dl2np.code_to_unittest import run
+
+
+def main():
+    config = get_config()
+    user_input = config["user_input"]
+    code_path = config["code_path"]
+    output_tag = config["output_tag"]
+    run(
+        user_input=user_input,
+        code_path=code_path,
+        output_tag=output_tag,
+    )
+
+
+if __name__ == "__main__":
+    main()
