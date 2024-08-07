f62f5528b9cfbe788f1bb02650af4ef863f3f927
JJ <jj.stankowicz@gmail.com>
2024-08-06 23:33:25 -0400

    Add Anthropic tests.

--- dl2np/tests/test_chatbots.py
+++ dl2np/tests/test_chatbots.py
@@ -1,9 +1,16 @@
 import pytest
-from dl2np.chatbots import openai
+from dl2np.chatbots import openai, claude
 
 
 def test_openai():
     out = openai("Hello, how are you?")
+    condition = isinstance(out, str)
+    error_str = "Expected type(out) == str. Received type(out) == {0}.".format(type(out))
+    assert condition, error_str
+
+
+def test_claude():
+    out = claude("Hello, how are you?")
     condition = isinstance(out, str)
     error_str = "Expected type(out) == str. Received type(out) == {0}.".format(type(out))
     assert condition, error_str
