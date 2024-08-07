810ecf50aea957e47de666402cc61d7ee218be3f
JJ <jj.stankowicz@gmail.com>
2024-08-07 01:39:14 -0400

    Change test for refactor into class.

--- dl2np/tests/test_chatbots.py
+++ dl2np/tests/test_chatbots.py
@@ -1,21 +1,15 @@
 import pytest
-from typing import Any, List
-from dl2np.chatbots import openai, anthropic, chat
+from typing import Any
+from dl2np.chatbots import chat
 
 MODEL_NAMES = ["gpt-4o-mini", "claude-3-haiku-20240307"]
 
-def _check_str(out: Any) -> bool:
+
+def _check_str(out: Any) -> None:
     condition = isinstance(out, str)
     error_str = f"Expected type(out) == str. Received type(out) == {type(out)}."
     assert condition, error_str
 
-def test_openai():
-    out = openai("Hello, how are you?")
-    _check_str(out)
-
-def test_claude():
-    out = anthropic("Hello, how are you?")
-    _check_str(out)
 
 @pytest.mark.parametrize("model_name", MODEL_NAMES)
 def test_chat(model_name: str):
