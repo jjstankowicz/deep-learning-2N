37aa92b7386ab75386e1b5f7cb6b42065737b8d6
JJ <jj.stankowicz@gmail.com>
2024-08-06 23:32:41 -0400

    Pass model name as arg for create_code.

--- dl2np/iac/create_code.py
+++ dl2np/iac/create_code.py
@@ -6,7 +6,12 @@ def main():
     config = get_config()
     user_input = config["user_input"]
     output_tag = config["output_tag"]
-    run(user_input, output_tag)
+    model_name = config["model_name"]
+    run(
+        user_input=user_input,
+        output_tag=output_tag,
+        model_name=model_name,
+    )
 
 
 if __name__ == "__main__":
