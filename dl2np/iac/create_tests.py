e69a6a2e44c639685d99a60fd2cca951f49fdf7a
JJ <jj.stankowicz@gmail.com>
2024-08-07 00:18:20 -0400

    Pass model_name through create_tests script.

--- dl2np/iac/create_tests.py
+++ dl2np/iac/create_tests.py
@@ -7,10 +7,12 @@ def main():
     user_input = config["user_input"]
     code_path = config["code_path"]
     output_tag = config["output_tag"]
+    model_name = config["model_name"]
     run(
         user_input=user_input,
         code_path=code_path,
         output_tag=output_tag,
+        model_name=model_name,
     )
 
 
