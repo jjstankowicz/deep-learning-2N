23e8e98a404994f6c4f8e461e877f0908d076be5
JJ <jj.stankowicz@gmail.com>
2024-08-07 00:18:49 -0400

    Add Claude 3.5

--- dl2np/dl2np/chatbots.py
+++ dl2np/dl2np/chatbots.py
@@ -30,9 +30,10 @@ def chat(user_input: str, model_name: str) -> str:
     return model(user_input=user_input, model_name=model_name)
 
 
-# 
+#
 # Open AI
-# 
+#
+
 
 class OpenAIModels(Enum):
     GPT_4O_MINI = "gpt-4o-mini"
@@ -92,7 +93,7 @@ class AnthropicModels(Enum):
     CLAUDE_3_OPUS = "claude-3-opus-20240229"
     CLAUDE_3_SONNET = "claude-3-sonnet-20240229"
     CLAUDE_3_HAIKU = "claude-3-haiku-20240307"
-
+    CLAUDE_35_SONNET = "claude-3-5-sonnet-20240620"
 
 
 def anthropic(
